baa434283a88bb8b60d0e32956728b1e5d0c166a
ZGuyDev <77798764+ZGuyDev@users.noreply.github.com>
2024-08-08 18:08:00 +0300

    Update __init__ to export RequestBuilder and callback_builders

--- aiohttp_utils/__init__.py
+++ aiohttp_utils/__init__.py
@@ -1,5 +1,9 @@
-from ._handler import RequestHandler
+from ._handler import (
+    RequestHandler,
+    CallbackBuilder,
+)
 from . import (
     response_callbacks as callbacks,
     response_models as models,
+    callback_builders as builders,
 )
