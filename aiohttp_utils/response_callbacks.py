6df1d0d5c92e27c17d90821f54a2fb21e3077237
ZGuyDev <77798764+ZGuyDev@users.noreply.github.com>
2024-08-08 21:45:28 +0300

    Add docstrings to response_callbacks

--- aiohttp_utils/response_callbacks.py
+++ aiohttp_utils/response_callbacks.py
@@ -4,7 +4,7 @@ from . import callback_builders as _cbb
 
 __all__ = (
     "status",
-    "info",
+    "properties",
     "read",
     "text",
     "json",
@@ -14,24 +14,82 @@ __all__ = (
 status = _cbuilder.build(
     _cbb.status,
 )
+"""
+`status` - return response status code and it's boolean representation
 
-info = _cbuilder.build(
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+"""
+
+properties = _cbuilder.build(
     _cbb.status,
     _cbb.headers,
     _cbb.cookies,
 )
+"""
+`properties` - return all property-like response fields: response status code,
+it's boolean representation, headers and cookies
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+    headers (CIMultiDictProxy[str]): inmutable case-insensitive multidict with
+    HTTP response headers.
+    cookies (http.cookies.SimpleCookie): container with HTTP response cookies.
+"""
 
 read = _cbuilder.develop(
-    info,
+    properties,
     _cbb.read,
 )
+"""
+`read` - return response body `bytes` and all property-like response fields
+(listed in `properties` response callback)
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+    headers (CIMultiDictProxy[str]): inmutable case-insensitive multidict with
+    HTTP response headers.
+    cookies (http.cookies.SimpleCookie): container with HTTP response cookies.
+    read (bytes): response body bytes
+"""
 
 text = _cbuilder.build(
-    info,
+    properties,
     _cbb.text,
 )
+"""
+`text` - return response body decoded as `str` and all property-like response
+fields (listed in `properties` response callback)
+
+Arguments:
+    text (**kwargs): kwargs to be unpacked as
+    `aiohttp.ClientResponse.text(**kwargs)`
+
+Returns:
+    status (str): response status code
+    ok (bool): response status code boolean representation.
+    `True` if status is less than 400; otherwise, `False`.
+    headers (CIMultiDictProxy[str]): inmutable case-insensitive multidict with
+    HTTP response headers.
+    cookies (http.cookies.SimpleCookie): container with HTTP response cookies.
+    text (str): response body decoded
+"""
 
 json = _cbuilder.build(
-    info,
+    properties,
     _cbb.json,
 )
+"""
+`json` - return response body with json, deserialized as a Python object, also
+all property-like response fields (listed in `properties` response callback)
+
+Arguments:
+    json (**kwargs): kwargs to be unpacked as
+    `aiohttp.ClientResponse.json(**kwargs)`
+"""
